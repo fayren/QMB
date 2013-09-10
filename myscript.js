b4a36147d12821837bd756b600f2291d821a31ad
fayren <phelanguan@gmail.com>
2013-09-10 19:26:41 -0400

    First Push
    Version 0.1

---
+++ myscript.js
@@ -0,0 +1,142 @@
+
+var saveFolder = 0; //counter to keep track of Saved Tabs folder status
+var foldId = 0; //var to hold the Saved Tabs folder Id
+var winId = 0 // var to hold recall window Id
+
+//listener for loading the extension. I'm actually not really sure what the wrapping listener does, just that it works.
+document.addEventListener('DOMContentLoaded', function () {
+	createFolder();	
+	console.log('started');
+	
+});
+
+//---------------------------------------------------------------------
+//initializ-ing
+
+//on plugin load, get the Bookmarks Tree and check through each node
+function createFolder() {
+	chrome.bookmarks.getTree(function(checkNode){
+		checkNode.forEach(function(item){
+			processNode(item);
+		});
+	console.log("Folders Checked");
+	console.log(saveFolder);
+    createFold();
+	});
+};
+
+//check to see if the Saved Tabs folder exists
+function processNode(node){
+	// checks each node if they have children, aka opens up all folders
+	if(node.children) {
+		node.children.forEach(function(child){
+			processNode(child);
+		});
+        // if the node doesn't have a URL, its a folder, and if it's title is Saved Tabs then we have our monkey
+		if(!node.url){
+		   if (node.title == 'Saved Tabs'){	
+			console.log("Saved Tabs folder exists!");
+			saveFolder = 1;
+			foldId = node.id;
+			console.log(saveFolder);
+			console.log(foldId);
+			}
+		}
+	}	
+};
+
+//create the Saved Tabs folder to store tabs
+function createFold(){
+    if (saveFolder == 0){
+		chrome.bookmarks.create({ 
+							title: 'Saved Tabs'}
+							);
+		console.log("added folder", Date.now());
+		saveFolder = 1;
+	}
+	
+};
+
+
+//------------------------------------------------------------
+//listeners
+
+//listener for keyboard shortcuts 
+chrome.commands.onCommand.addListener(function(command) {
+  console.log('listening');
+  //save current tab in bookmarks, 
+  if (command == 'cns-tab'){
+	cns();
+  }
+  //open all saved tabs in a new window
+  else if(command == 'win-recall') {
+    
+	chrome.bookmarks.getChildren(foldId, function(check){
+		console.log(check);
+		if (check.length > 1){
+			windowRecall();
+		}
+		else {
+			alert('There are no saved tabs at this time!')
+		}
+	});
+	
+	
+	}
+  //close the magic window and save all tabs 
+  else if (command == 'win-dump') {
+	windowDump();
+  }
+});
+
+//-------------------------------------------------------------
+//functions for shortcuts
+
+function cns(){
+	chrome.tabs.getSelected(null, function(tab){
+		console.log(tab);
+		chrome.bookmarks.create({
+			parentId: foldId,
+			title: tab.title, 
+			url: tab.url
+		});
+		chrome.tabs.remove(tab.id, function(rtab){
+			console.log('saved and closed');
+		});
+	
+    });
+};
+
+function windowRecall(){
+// need try catch; can't try to recall empty folder 
+	chrome.bookmarks.getChildren(foldId.toString(), function(sFolder){
+	
+		chrome.windows.create({}, function(window){
+			console.log(window.id);
+			winId = window.id;
+			sFolder.forEach(function(urlify){
+				chrome.tabs.create({windowId:window.id, url:urlify.url})
+				chrome.bookmarks.remove(urlify.id);
+			});
+			chrome.tabs.query({windowId: window.id, index:0}, function(nullId){
+				chrome.tabs.remove(nullId[0].id);
+			});
+		});		
+	});	
+};
+
+function windowDump(){
+	chrome.windows.get(winId, {populate: true}, function(winTabs){
+		winTabs.tabs.forEach(function(resave){
+			console.log(resave.url);
+			chrome.bookmarks.create({
+				parentId: foldId,
+				title: resave.title, 
+				url: resave.url
+			});
+		});
+	});
+	
+	chrome.windows.remove(winId);
+};
+
